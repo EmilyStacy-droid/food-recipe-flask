aaacf982eb15b3d8efadf0b17718135dd7040e25
EmilyStacy <emily.stacy@resi.io>
2024-05-09 21:00:40 -0500

    add monitoring

---
+++ README.md
@@ -0,0 +1,2 @@
+# Set up
+docker build -t my-flask-app .
--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from prometheus_client import Counter
 
 app = Flask(__name__, template_folder='templates')
 
-# @app.route("/")
-# def main():
-#     return render_template('index.html')
+# Create Prometheus counters for views and purchases
+view_metric = Counter('view_total', 'Homepage view', ['page'])
 
 @app.route("/", methods=["GET"])
 def main():
+    view_metric.labels("homepage").inc()
     return render_template('index.html')
 
 @app.route("/echo_user_input", methods=["POST"])
@@ -18,5 +19,10 @@ def echo_input():
     returned_str = f"You want to have a {cuisine} cusine with min {min_calaries} calaries"
     return "You entered: " + returned_str
 
+
+@app.route("/health", methods=["GET"])
+def get_health():
+    return jsonify({"status":"OK"},200)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
